995f14d7d9bcef514da6672580e3ff1681b19f4c
Adit <adithernowo@gmail.com>
2024-04-19 16:31:48 +0700

    add eco config json

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apps: [{
-        name: "my_uv_app",
-        script: "uvicorn",
-        args: ["main:app", "--reload"],
-        instances: "max",
-        exec_mode: "cluster"
-    }]
-}
